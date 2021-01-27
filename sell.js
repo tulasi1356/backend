dd29eb9f5c8e8ce77b20c32949574c88a4338cc7
tulasi1356 <botchatulasi1356@gmail.com>
2021-01-27 12:52:00 +0530

    seperation of backend and fe

--- sell.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const router=express.Router()
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-     dbo = db.db("learningbackend");
-     
-  })
-  
