65a3a8749e238ee01c80e352520d3365576410e5
tulasi1356 <botchatulasi1356@gmail.com>
2021-01-27 12:41:22 +0530

    seperating backend and frontend

---
+++ back1/sell.js
@@ -0,0 +1,8 @@
+// const express=require('express')
+// const router=express.Router()
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//      dbo = db.db("learningbackend");
+     
+//   })
+module.exports = router;
