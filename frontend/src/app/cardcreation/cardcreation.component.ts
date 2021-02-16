50ca0b734588068f68abb7837529ea791fb2a119
tulasi1356 <botchatulasi1356@gmail.com>
2021-02-16 23:36:11 +0530

    changes in cardcreation

--- frontend/src/app/cardcreation/cardcreation.component.ts
+++ frontend/src/app/cardcreation/cardcreation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SellserviceService} from '../../services/sellservice.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cardcreation',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cardcreation.component.scss']
 })
 export class CardcreationComponent implements OnInit {
-
-  constructor() { }
+public finalcarddetails;
+  constructor(private router:Router,private sell:SellserviceService,private snackbar: MatSnackBar,) { }
 
   ngOnInit(): void {
+    this.sell.cardcreationdetails().subscribe(
+      data=>{
+        this.finalcarddetails=data;
+      }
+    )
   }
 
 }
