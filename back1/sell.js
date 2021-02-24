1bc9cbf5b59288a4dcd32fa28a0386db2553d097
tulasi1356 <botchatulasi1356@gmail.com>
2021-02-24 21:33:36 +0530

    push the master

--- back1/sell.js
+++ back1/sell.js
@@ -13,16 +13,16 @@ MongoClient.connect(url, function(err, db) {
      
   })
   // // USED TO DELETE DATA IN COLLECTION
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("learningbackend");
-    //Delete the "customers" collection:
-    dbo.collection("sellformdetails").drop(function(err, delOK) {
-      if (err) throw err;
-      if (delOK) console.log("Collection deleted");
-      db.close();
-    });
-  });
+  // MongoClient.connect(url, function(err, db) {
+  //   if (err) throw err;
+  //   var dbo = db.db("learningbackend");
+  //   //Delete the "customers" collection:
+  //   dbo.collection("sellformdetails").drop(function(err, delOK) {
+  //     if (err) throw err;
+  //     if (delOK) console.log("Collection deleted");
+  //     db.close();
+  //   });
+  // });
   router.post('/sellformdetails',function(req,res){
       console.log(req.session.details);
   myobj={userid:req.session.details.id,companyname:req.body.companyname,productname:req.body.productname,category:req.body.category,type:req.body.type,brand:req.body.brand,color:req.body.color,realprize:req.body.realprize,discount:req.body.discount,age:req.body.age,size:req.body.size,noofproduct:req.body.noofproducts,image:req.body.image,place:req.body.place,videolink:req.body.videolink};
